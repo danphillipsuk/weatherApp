0626afd60daea52c6902784f70edc1077b64cab6
Daniel Phillips <dan.phillips100@gmail.com>
2022-02-19 11:44:36 +0000

    OpenWeather API module complete

--- src/index.js
+++ src/index.js
@@ -2,6 +2,71 @@ import _ from 'lodash';
 import './style.css';
 import { getWeather } from './weatherAPI.js';
 
-// getWeather();
+const currentWeather = (city, oneCall) => {
+  console.log(oneCall)
+  console.log(city)
 
-console.log("hello from index.js")
+  const overallWeather = document.getElementById("currentWeather");
+  if (oneCall.current.weather[0].id == 803) {
+    overallWeather.classList.add("thunderstorm");
+  }
+
+//   // localStorage.setItem("weatherObj", JSON.stringify(oneCall));
+//   // const workingWeatherObj = JSON.parse(localStorage.getItem("weatherObj"));
+//   // console.log(workingWeatherObj)
+//   // console.log(oneCall)
+
+//   // console.log(oneCall.current.temp) //curent temp
+//   // console.log(oneCall.current.feels_like) //feels like
+
+  const windSpeed = oneCall.current.wind_speed; //wind speed
+  const windGusts = oneCall.current.wind_gust; //wind gust
+
+  let num = oneCall.current.wind_deg
+
+  function degToCompass(num) {
+    var val = Math.floor((num / 22.5) + 0.5);
+    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+    return arr[(val % 16)];
+  }
+
+  const windDirection = degToCompass(num);
+
+  const wind = `${windDirection} @ ${windSpeed} km/hr`;
+
+  console.log(`${windDirection} @ ${windSpeed}km/hr. Gusting at ${windGusts}km/hr`)
+
+//   // console.log(oneCall.current.visibility) // visibility
+//   // console.log(oneCall.current.weather[0].description) // visibility
+
+  const userCity = document.getElementById("city");
+  userCity.innerText = city;
+
+  const currentTemp = document.getElementById("temp");
+  currentTemp.innerText = Math.round(oneCall.current.temp);
+
+  const currentWeath = document.getElementById("current");
+  currentWeath.innerText = oneCall.current.weather[0].description;
+
+  const currentWind = document.getElementById("wind");
+  currentWind.innerText = wind;
+
+  if (oneCall.alerts != null) {
+    const warning = document.createElement("div");
+    warning.classList.add("warning");
+    warning.innerText = `${oneCall.alerts[0].event}`;
+    const container = document.getElementById("currentWeather");
+    container.append(warning);
+  }
+
+  const sun = new Date(oneCall.current.sunset *1000);
+  const sunsetTime = sun.toLocaleTimeString();
+  const sunset = document.getElementById("sunset");
+  sunset.innerText = `The sun will set at ${sunsetTime}`;
+  
+
+
+}
+
+
+export { currentWeather }
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,54 +1,36 @@
-// function apiCalls () {
-
+import { propertyOf } from "lodash";
+import { currentWeather } from './index.js';
+let today;
 let oneCall;
+let city;
 
-async function getWeather () {
+async function getWeather (location) {
+
+  if (location == null) {
+    location = "London";
+  }
 
   try {
     // Get current weather report to give us city name and lat/lon coordinates
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Rothbury&units=metric&APPID=5a75251970a67885f4e3d704fe65eed0`, { mode: 'cors' });
-    const weatherData = await response.json();
-    const lat = weatherData.coord.lat; 
-    const lon = weatherData.coord.lon;
-    const city = weatherData.name;
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=5a75251970a67885f4e3d704fe65eed0`, { mode: 'cors' });
+    today = await response.json();
+    const lat = today.coord.lat; 
+    const lon = today.coord.lon;
+    city = today.name;
+    console.log(today)
 
     // Use lat/lon cordinates from first fetch to pass into oneCall service, giving us current and forecast data
     const secondResponse = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&APPID=5a75251970a67885f4e3d704fe65eed0`, { mode: 'cors' });
     oneCall = await secondResponse.json();
-    console.log(oneCall)
       
   } catch(err) {
     console.log(err)
   }
 }
 
-
-getWeather().then(function tryThis() {
-  console.log(oneCall)
-});
-
-
-
-// const weatherFactory = (city, weather, currentTemp) => {
-//   return { city, weather, currentTemp };
-// }
-
-// const today = weatherFactory('rothbury', 'Clear & sunny', 21);
-
-// //   let num = data.wind.deg
-
-// //   function degToCompass(num) {
-// //     var val = Math.floor((num / 22.5) + 0.5);
-// //     var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-// //     console.log(arr[(val % 16)]);
-// //     return arr[(val % 16)];
-// // }
-// //   degToCompass(num);
-
-
-
-
-
+getWeather().then(function () {
+  currentWeather(city, oneCall);
+})
 
 export { getWeather }
 
