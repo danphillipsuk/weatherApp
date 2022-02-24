566e87f9c531d7f2f62f16843940e31f9280c28c
Daniel Phillips <dan.phillips100@gmail.com>
2022-02-24 12:34:35 +0000

    Changed API call HTTPS

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -12,13 +12,13 @@ async function getWeather (location) {
 
   try {
     // Get current weather report to give us city name and lat/lon coordinates
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=5a75251970a67885f4e3d704fe65eed0`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=5a75251970a67885f4e3d704fe65eed0`, { mode: 'cors' });
     today = await response.json();
     const lat = today.coord.lat; 
     const lon = today.coord.lon;
 
     // Use lat/lon cordinates from first fetch to pass into oneCall service, giving us current and forecast data
-    const secondResponse = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&APPID=5a75251970a67885f4e3d704fe65eed0`, { mode: 'cors' });
+    const secondResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&APPID=5a75251970a67885f4e3d704fe65eed0`, { mode: 'cors' });
     oneCall = await secondResponse.json();
 
     construct(today, oneCall);
